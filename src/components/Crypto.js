349a7f5c6f870e04ad079449f0406e35607af0e9
patrick thunder <patrickthunder@gmail.com>
2018-05-22 13:29:50 -0700

    Created Crypto Child Component
    Created Crypto Child Component: Added 7 variables for the values inside this.props.data, price at 100 at 2 nearest decimal places. Set props for data.
    Styling

---
+++ src/components/Crypto.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+import './Crypto.css';
+
+class Crypto extends Component {
+    
+    render () {
+        const {
+            id,
+            name,
+            symbol,
+            price_usd,
+            percent_change_1h,
+            percent_change_24h,
+            percent_change_7d,
+        } = this.props.data;
+        
+        return ( 
+            <li className = {"cryptocurrency " + id}>
+                <p className = "cryptocurrency-name"> {name}({symbol}) </p> 
+                <h1> $ {(+price_usd).toFixed(2)}</h1> 
+                <p> {percent_change_1h}% 1 hr</p>
+                <p> {percent_change_24h}% 24 hrs</p> 
+                <p> {percent_change_7d}% 7 days</p> 
+            </li>
+        );
+
+    }
+
+}
+
+export default Crypto;
+
+{/* Child Component
+     7 variables for the values inside this.props.data, 
+     price at 100 at 2 nearest fixed decimal places
+*/}
--- src/components/Tickers.js
+++ src/components/Tickers.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './Tickers.css';
+import Crypto from './Crypto.js';
 
 class Tickers extends Component {
     
@@ -7,28 +8,48 @@ class Tickers extends Component {
         super(props);
         this.state = {
             data: [
-                {
-                    id: "bitcoin",
-                    price: "1"
-                },
-                {
-                    id: "ethereum",
-                    price: "1"
-                },
-                {
-                    id: "litecoin",
-                    price: "1"
-                }
+               {
+                   id: "bitcoin",
+                   name: "Bitcoin",
+                   symbol: "BTC",
+                   price_usd: "1",
+                   percent_change_1h: "0",
+                   percent_change_24h: "0",
+                   percent_change_7d: "0",
+               }, {
+                   id: "ethereum",
+                   name: "Ethereum",
+                   symbol: "ETH",
+                   price_usd: "1",
+                   percent_change_1h: "0",
+                   percent_change_24h: "0",
+                   percent_change_7d: "0",
+               }, 
+               {
+                   id: "ripple",
+                   name: "Ripple",
+                   symbol: "XRP",
+                   price_usd: "1",
+                   percent_change_1h: "0",
+                   percent_change_24h: "0",
+                   percent_change_7d: "0",
+               },
+               {
+                   id: "litecoin",
+                   name: "Litecoin",
+                   symbol: "LTC",
+                   price_usd: "1",
+                   percent_change_1h: "0",
+                   percent_change_24h: "0",
+                   percent_change_7d: "0",
+               }
             ]
         };
     }
 
     render() {
-        var tickers = this.state.data.map((currency) =>
-            <li key={currency.id}>
-                <h3>{currency.id}</h3> 
-                <h4>{currency.price} USD</h4> 
-            </li>
+        let tickers = this.state.data.map((currency) =>
+             <Crypto data = {currency} key = {currency.id}/>
         );
 {/* key property is needed for react to keep track of each li */ }
         return ( 
@@ -45,3 +66,5 @@ class Tickers extends Component {
 {/* Comments:
 The js map function inside state to iterate through each crypto creating li elements
 */ }
+
+{ /* data is property containing all of the data needed for a single cryptocurrency, to pass in from the parent component */ }
